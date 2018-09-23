0e970b773ba0578ad2748d28bf6d25fb2aac212a
Rock Hymas <rock@hymasfamily.org>
2018-09-23 11:15:16 -0600

    Code cleanup: sidebar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,79 +6,20 @@
       <userwidget/>
     </div>
     <b-collapse is-nav id="nav_collapse">
-      <div class="sidebar">
-        <div>
-        <b-card class="today-card" no-body>
-          <b-list-group flush>
-          <b-list-group-item :variant="currentDayPlanId === primaryListId ? 'primary' : ''">
-            <a @click.prevent="switchList({ namespace: 'primaryList', listId: currentDayPlanId })" href="#">{{ $store.getters.currentDayPlanName }}</a>
-          </b-list-group-item>
-          <b-list-group-item v-if="tomorrowDayPlanId" :variant="tomorrowDayPlanId === primaryListId ? 'primary' : ''">
-            <a @click.prevent="switchList({ namespace: 'primaryList', listId: tomorrowDayPlanId })" href="#">{{ $store.getters.tomorrowDayPlanName }}</a>
-          </b-list-group-item>
-          </b-list-group>
-          <b-button v-if="!(tomorrowDayPlanId)" variant="link" @click.prevent="startDayPlan">Plan {{ dayPlanIsCurrent ? 'Tomorrow' : 'Today' }}</b-button>
-        </b-card>
-        <b-card no-body>
-          <b-tabs card v-model="collection">
-          <b-tab title="Current">
-            <listlist :lists="activeLists"/>
-          </b-tab>
-          <b-tab title="Archive">
-            <listlist :lists="archiveLists"/>
-          </b-tab>
-          </b-tabs> 
-        </b-card>
-        </div>
-      </div>
+      <sidebar/>
     </b-collapse>
   </b-navbar>
 </template>
 
 <script>
-import ListList from './ListList.vue'
 import UserWidget from './UserWidget.vue'
-import draggable from 'vuedraggable'
-import { mapGetters, mapActions } from 'vuex'
+import Sidebar from './Sidebar.vue'
 
 export default {
   name: 'navbar',
   components: {
-    listlist: ListList,
     userwidget: UserWidget,
-    draggable
-  },
-  data () {
-    return {
-      collection: 0
-    }
-  },
-  computed: {
-    primaryListId: function () {
-      return this.$store.getters['primaryList/id']
-    },
-    ...mapGetters([
-      'activeLists',
-      'archiveLists',
-      'currentDayPlanId',
-      'dayPlanIsCurrent',
-      'tomorrowDayPlanId'
-    ])
-  },
-  mounted () {
-    this.$store.dispatch('loadLists')
-  },
-  methods: {
-    ...mapActions([
-      'switchList',
-      'startDayPlan'
-    ])
+    sidebar: Sidebar
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.today-card {
-  margin-bottom: 20px;
-}
-</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="sidebar">
+    <b-card class="today-card" no-body>
+      <b-list-group flush>
+      <b-list-group-item :variant="currentDayPlanId === primaryListId ? 'primary' : ''">
+        <a @click.prevent="switchList({ namespace: 'primaryList', listId: currentDayPlanId })" href="#">{{ $store.getters.currentDayPlanName }}</a>
+      </b-list-group-item>
+      <b-list-group-item v-if="tomorrowDayPlanId" :variant="tomorrowDayPlanId === primaryListId ? 'primary' : ''">
+        <a @click.prevent="switchList({ namespace: 'primaryList', listId: tomorrowDayPlanId })" href="#">{{ $store.getters.tomorrowDayPlanName }}</a>
+      </b-list-group-item>
+      </b-list-group>
+      <b-button v-if="!(tomorrowDayPlanId)" variant="link" @click.prevent="startDayPlan">Plan {{ dayPlanIsCurrent ? 'Tomorrow' : 'Today' }}</b-button>
+    </b-card>
+    <b-card no-body>
+      <b-tabs card v-model="collection">
+      <b-tab title="Current">
+        <listlist :lists="activeLists"/>
+      </b-tab>
+      <b-tab title="Archive">
+        <listlist :lists="archiveLists"/>
+      </b-tab>
+      </b-tabs> 
+    </b-card>
+  </div>
+</template>
+
+<script>
+import ListList from './ListList.vue'
+import draggable from 'vuedraggable'
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'sidebar',
+  components: {
+    listlist: ListList,
+    draggable
+  },
+  data () {
+    return {
+      collection: 0
+    }
+  },
+  computed: {
+    primaryListId: function () {
+      return this.$store.getters['primaryList/id']
+    },
+    ...mapGetters([
+      'activeLists',
+      'archiveLists',
+      'currentDayPlanId',
+      'dayPlanIsCurrent',
+      'tomorrowDayPlanId'
+    ])
+  },
+  mounted () {
+    this.$store.dispatch('loadLists')
+  },
+  methods: {
+    ...mapActions([
+      'switchList',
+      'startDayPlan'
+    ])
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.today-card {
+  margin-bottom: 20px;
+}
+</style>
